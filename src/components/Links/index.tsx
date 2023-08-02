eb6772edcc32af9c95f8cc0dcc4f004b55d30067
marcin10lw <marcin10lw@wp.pl>
2023-08-02 12:04:47 +0200

    created Links component

---
+++ src/components/Links/index.tsx
@@ -0,0 +1,28 @@
+import styles from './index.module.scss';
+import { ReactComponent as GithubIcon } from 'assets/images/icon-github.svg';
+import { ReactComponent as FrontendMentorIcon } from 'assets/images/icon-frontend-mentor.svg';
+import { ReactComponent as LinkedinIcon } from 'assets/images/icon-linkedin.svg';
+
+const Links = () => {
+  return (
+    <ul className={styles.linksList}>
+      <li>
+        <a href="https://github.com/marcin10lw" target="_blank" rel="noopener noreferrer">
+          <GithubIcon />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.frontendmentor.io/profile/marcin10lw" target="_blank" rel="noopener noreferrer">
+          <FrontendMentorIcon />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/marcin-augun-951659266/" target="_blank" rel="noopener noreferrer">
+          <LinkedinIcon />
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default Links;
