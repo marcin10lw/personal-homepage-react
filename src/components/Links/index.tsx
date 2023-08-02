75dab09b61251f128e66497667c15639ae1632fe
marcin10lw <marcin10lw@wp.pl>
2023-08-02 14:03:17 +0200

    styled header mobile view

--- src/components/Links/index.tsx
+++ src/components/Links/index.tsx
@@ -1,24 +1,24 @@
 import styles from './index.module.scss';
-import { ReactComponent as GithubIcon } from 'assets/images/icon-github.svg';
-import { ReactComponent as FrontendMentorIcon } from 'assets/images/icon-frontend-mentor.svg';
-import { ReactComponent as LinkedinIcon } from 'assets/images/icon-linkedin.svg';
+import { AiFillGithub } from 'react-icons/ai';
+import { SiFrontendmentor } from 'react-icons/si';
+import { BsLinkedin } from 'react-icons/bs';
 
 const Links = () => {
   return (
     <ul className={styles.linksList}>
       <li>
         <a href="https://github.com/marcin10lw" target="_blank" rel="noopener noreferrer">
-          <GithubIcon className={styles.linksList__icon} />
+          <AiFillGithub className={styles.linksList__icon} />
         </a>
       </li>
       <li>
         <a href="https://www.frontendmentor.io/profile/marcin10lw" target="_blank" rel="noopener noreferrer">
-          <FrontendMentorIcon className={styles.linksList__icon} />
+          <SiFrontendmentor className={styles.linksList__icon} />
         </a>
       </li>
       <li>
         <a href="https://www.linkedin.com/in/marcin-augun-951659266/" target="_blank" rel="noopener noreferrer">
-          <LinkedinIcon className={styles.linksList__icon} />
+          <BsLinkedin className={styles.linksList__icon} />
         </a>
       </li>
     </ul>
