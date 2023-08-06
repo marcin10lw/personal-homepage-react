70aa751286b40611072c064b0f6b20eca0cfbebe
marcin10lw <marcin10lw@wp.pl>
2023-08-06 12:51:27 +0200

    added focus animation and fade in links

--- src/components/Projects/Project/index.tsx
+++ src/components/Projects/Project/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import image from 'assets/images/thumbnail-project-1-large.webp';
 import styles from './index.module.scss';
 
@@ -8,10 +10,24 @@ type ProjectProps = {
 };
 
 const Project = ({ name, codeUrl, liveUrl }: ProjectProps) => {
+  const [isLinkFocused, setIsLinkFocused] = useState(false);
+
+  const onLinkFocus = () => {
+    setIsLinkFocused(true);
+  };
+
+  const onLinkBlur = () => {
+    setIsLinkFocused(false);
+  };
+
   return (
     <article>
       <div className={styles.project}>
-        <div className={styles.project__image}>
+        <div
+          className={`${styles.project__image} ${
+            isLinkFocused ? styles['project__image--focused'] : ''
+          }`}
+        >
           <img src={image} alt={name} />
         </div>
         <h3 className={styles.project__name}>{name}</h3>
@@ -19,12 +35,30 @@ const Project = ({ name, codeUrl, liveUrl }: ProjectProps) => {
           <li>HTML</li>
           <li>CSS</li>
         </ul>
-        <div className={styles.project__links}>
-          <a className="button" href={codeUrl} target="_blank" rel="noopener noreferrer">
-            view code
-          </a>
-          <a className="button" href={liveUrl} target="_blank" rel="noopener noreferrer">
+        <div
+          className={`${styles.project__links} ${
+            isLinkFocused ? styles['project__links--focused'] : ''
+          }`}
+        >
+          <a
+            className="button"
+            href={liveUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            onFocus={onLinkFocus}
+            onBlur={onLinkBlur}
+          >
             view peoject
+          </a>
+          <a
+            className="button"
+            href={codeUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            onFocus={onLinkFocus}
+            onBlur={onLinkBlur}
+          >
+            view code
           </a>
         </div>
       </div>
