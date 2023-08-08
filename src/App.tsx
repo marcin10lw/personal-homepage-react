31f6415042bdcf5c59e7d56f81eccead66610347
marcin10lw <marcin10lw@wp.pl>
2023-08-08 09:43:03 +0200

    created Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from 'components/Header';
 import Hero from 'components/Hero';
 import Skills from 'components/Skills';
 import Projects from 'components/Projects';
+import Footer from 'components/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Skills />
         <Projects />
       </main>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,66 @@
+import styles from './index.module.scss';
+
+const Footer = () => {
+  return (
+    <div className={styles.footerWrapper}>
+      <footer className={`container`}>
+        <div className={styles.footer__mainContent}>
+          <article className={styles.footer__article}>
+            <h2 className="heading">Contact</h2>
+            <p>
+              I would love to hear about your project and how I could help.
+              Please fill in the form, and I’ll get back to you as soon as
+              possible.
+            </p>
+          </article>
+
+          <form>
+            <fieldset className={styles.form__fieldset}>
+              <div className={styles.input}>
+                <label htmlFor="name">name</label>
+                <input
+                  id="name"
+                  type="text"
+                  name="name"
+                  placeholder="NAME"
+                  required
+                />
+                <span className={styles.input__error} />
+                <div className={styles.input__errorIcon} />
+              </div>
+              <div className={styles.input}>
+                <label htmlFor="email">email</label>
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="EMAIL"
+                  required
+                />
+                <span className={styles.input__error} />
+                <div className={styles.input__errorIcon} />
+              </div>
+              <div className={styles.input}>
+                <label htmlFor="message">email</label>
+                <textarea
+                  id="message"
+                  name="message"
+                  placeholder="MESSAGE"
+                  required
+                />
+                <span className={styles.input__error} />
+                <div className={styles.input__errorIcon} />
+              </div>
+            </fieldset>
+
+            <button className={`button ${styles.form__button}`}>
+              send message
+            </button>
+          </form>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
