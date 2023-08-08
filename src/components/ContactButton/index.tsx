5b76794bbe80833646c985f3abcf68798827075c
marcin10lw <marcin10lw@wp.pl>
2023-08-08 23:01:11 +0200

    replaced .button class with button mixin

--- src/components/ContactButton/index.tsx
+++ src/components/ContactButton/index.tsx
@@ -1,5 +1,7 @@
+import styles from './index.module.scss';
+
 const ContactButton = () => {
-  return <button className="button">contact me</button>;
+  return <button className={styles.contactButton}>contact me</button>;
 };
 
 export default ContactButton;
