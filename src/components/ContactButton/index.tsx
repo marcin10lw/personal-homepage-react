25755294cb5dbc025c9e3dfc72c5ef35291439e5
marcin10lw <marcin10lw@wp.pl>
2023-08-08 23:27:52 +0200

    added scrolling to footer on contact me click

--- src/components/ContactButton/index.tsx
+++ src/components/ContactButton/index.tsx
@@ -1,7 +1,19 @@
 import styles from './index.module.scss';
 
 const ContactButton = () => {
-  return <button className={styles.contactButton}>contact me</button>;
+  const onButtonClick = () => {
+    const footerElement = document.querySelector<HTMLElement>('#footer');
+
+    if (footerElement) {
+      footerElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <button onClick={onButtonClick} className={styles.contactButton}>
+      contact me
+    </button>
+  );
 };
 
 export default ContactButton;
