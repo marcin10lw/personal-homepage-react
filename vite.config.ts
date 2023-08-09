91c4c1de84421516c16c36ccdfb5f98f07563a5f
marcin10lw <marcin10lw@wp.pl>
2023-08-10 00:15:37 +0200

    setup baseUrl and deploy scripts

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,5 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
+  base: 'https://marcin10lw.github.io/personal-homepage-react/',
 });
